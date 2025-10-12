57f608343c74a7aa4746b95fa535bb6ee987f970
Minseok Choe <choems@outlook.com>
2025-10-13 02:57:29 +0900

    fix: use modern configs

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,68 +1,101 @@
+local helper = require "helper.lsp"
+
 local servers = {
-  "html",
-  "cssls",
-  "eslint",
-  "lua_ls",
-  "solidity",
-  "dockerls",
-  "gopls",
-  "intelephense",
-  "ansiblels",
-  "dartls",
-  "pylsp",
-  "pbls",
-  "metals",
-  {
-    "rust_analyzer",
-    config = {
-      settings = {
-        ["rust-analyzer"] = {
-          checkOnSave = {
-            command = "clippy",
-          },
+  html = {},
+  cssls = {},
+  eslint = {},
+  lua_ls = {},
+  solidity = {},
+  dockerls = {},
+  gopls = {},
+  intelephense = {},
+  ansiblels = {},
+  dartls = {},
+  pylsp = {},
+  pbls = {},
+  metals = {},
+  rust_analyzer = {
+    settings = {
+      ["rust-analyzer"] = {
+        checkOnSave = {
+          command = "clippy",
         },
       },
     },
   },
-  {
-    "ts_ls",
-    config = {
-      settings = {
-        typescript = {
-          preferences = {
-            importModuleSpecifier = "project-relative"
-          }
-        }
+  ts_ls = {
+    settings = {
+      typescript = {
+        preferences = {
+          importModuleSpecifier = "project-relative",
+        },
       },
-    }
+    },
   },
 }
 
+local function default_on_attach(client, bufnr)
+  helper.on_attach(client.id, bufnr)
+end
+
+local function should_disable(server_opts)
+  if server_opts == false then
+    return true
+  end
+
+  if type(server_opts) == "table" and server_opts.enabled == false then
+    return true
+  end
+
+  return false
+end
+
 return {
   "neovim/nvim-lspconfig",
-  event = { "BufReadPost", "BufNewFile", "BufWritePre" },
-  config = function()
-    local lspconfig = require "lspconfig"
-    local capabilities = require("helper.lsp").capabilities
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    servers = servers,
+    setup = {},
+  },
+  config = function(_, opts)
+    local on_attach = opts.on_attach or default_on_attach
+    local base_capabilities = opts.capabilities or helper.capabilities
 
-    for _, lsp in ipairs(servers) do
-      local config = { capabilities = capabilities }
-      local name = lsp
-
-      if type(lsp) == "table" then
-        name = lsp[1]
-        config = vim.tbl_deep_extend("force", config, lsp.config)
+    local function setup(server, server_opts)
+      if should_disable(server_opts) then
+        return
       end
 
-      lspconfig[name].setup(config)
+      local defaults = {
+        on_attach = on_attach,
+      }
+
+      if base_capabilities then
+        defaults.capabilities = vim.deepcopy(base_capabilities)
+      end
+
+      local merged_opts = vim.tbl_deep_extend("force", defaults, type(server_opts) == "table" and server_opts or {})
+
+      if merged_opts.enabled ~= nil then
+        merged_opts.enabled = nil
+      end
+
+      if opts.setup and opts.setup[server] then
+        if opts.setup[server](server, merged_opts) then
+          return
+        end
+      elseif opts.setup and opts.setup["*"] then
+        if opts.setup["*"](server, merged_opts) then
+          return
+        end
+      end
+
+      vim.lsp.config(server, merged_opts)
+      vim.lsp.enable(server)
     end
 
-    vim.api.nvim_create_autocmd("LspAttach", {
-      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-      callback = function(args)
-        local on_lsp_attach = require("helper.lsp").on_attach
-        on_lsp_attach(args.data.client_id, args.buf)
-      end,
-    })
+    for server, server_opts in pairs(opts.servers or {}) do
+      setup(server, server_opts)
+    end
   end,
 }
