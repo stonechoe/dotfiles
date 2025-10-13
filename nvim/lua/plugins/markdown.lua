55faf8abd58fdb76e8d92176efbca2bb5d81f8e6
Minseok Choe <choems@outlook.com>
2025-10-13 13:43:30 +0900

    fix: markdown

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,6 +1,6 @@
 return {
   'MeanderingProgrammer/markdown.nvim',
-  dependencies = { 'nvim-treesitter' },
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
   config = function()
     require('render-markdown').setup({
     })
